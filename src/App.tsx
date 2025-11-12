be257a4fd8b18bf1940110e19f4fbbf4731b3161
KyanKersten <117643959+KyanKersten@users.noreply.github.com>
2025-11-12 10:44:12 +0100

    fix: Updated wrong import directoryx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from './components/Header/Header';
+import Header from './components/Header';
 import ColorGrid from './components/ColorGrid';
 import Footer from './components/Footer';
 
