e61ddbe864cdcd9ca31840e3d6491c075e458a5b
Kyan Kersten <KJM.kersten@student.han.nl>
2026-03-16 09:27:45 +0100

    CI Test

--- src/components/EndScore.tsx
+++ src/components/EndScore.tsx
@@ -20,7 +20,7 @@ export default function EndScore({
       ? (reactionTimes.reduce((a, b) => a + b, 0) / reactionTimes.length / 1000).toFixed(2)
       : "-";
 
-      // Pr test
+      // LLALALALALAAL 
   return (
     <div
       role="dialog"
